390ee08f362282f7835b1c5ca13f821c79a6dad6
adamgofast <adam@gofastcrushgoals.com>
2025-06-07 17:32:20 +0000

    index fixes

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { TripProvider } from "./context/TripContext"; // ✅ import context
-import "./index.css"; // ✅ if you're using Tailwind or styles
+import { TripProvider } from "./context/TripContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<TripProvider> {/* ✅ wrap app in trip context */}
+		<TripProvider>
 			<App />
 		</TripProvider>
 	</React.StrictMode>
