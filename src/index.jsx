cf90d98f07c5948d882e05404bf84a7f87938d96
adamgofast <adam@gofastcrushgoals.com>
2025-06-08 19:35:08 +0000

    index fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,21 @@
+// src/index.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { TripProvider } from "./context/TripContext";
+import { BrowserRouter } from "react-router-dom";
+
+// 🚀 This is where your app starts
+// 1. Wrap everything in <BrowserRouter> so routing works
+// 2. Then wrap in <TripProvider> so we can hydrate user + trip
+// 3. Finally render the main <App /> with all your pages
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<TripProvider>
-			<App />
-		</TripProvider>
+		<BrowserRouter>          {/* Step 1: Makes routing possible */}
+			<TripProvider>         {/* Step 2: Authenticates + loads trip data */}
+				<App />              {/* Step 3: Renders routes like /, /explainer */}
+			</TripProvider>
+		</BrowserRouter>
 	</React.StrictMode>
 );
