b0a8ebffcabf72902652bd9d8d9adcdc6704f2f3
adamgofast <adam@gofastcrushgoals.com>
2025-08-05 21:40:25 +0000

    index

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 // src/index.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css"; // ✅ Loads Tailwind styles
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
